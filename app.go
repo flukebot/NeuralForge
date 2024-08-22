4cb66089d364578d96ea670bb8dae9ba81f99116
Samuel Watson <planetbridging@gmail.com>
2024-08-23 09:04:30 +1000

    created dual boot mode

--- app.go
+++ app.go
@@ -1,105 +1,95 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
+    "context"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "path/filepath"
 )
 
-// App struct
 type App struct {
-	ctx context.Context
+    ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+    return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
-	a.ctx = ctx
+    a.ctx = ctx
 
-	// Ensure the NeuralForge folder structure exists
-	projectDir, err := a.CreateProjectFolder()
-	if err != nil {
-		fmt.Println("Error creating NeuralForge directory:", err)
-		return
-	}
+    // Initialize project directories
+    projectDir, err := a.CreateProjectFolder()
+    if err != nil {
+        fmt.Println("Error creating NeuralForge directory:", err)
+        return
+    }
 
-	// Ensure the "projects" subdirectory exists
-	projectsDir := filepath.Join(projectDir, "projects")
-	err = os.MkdirAll(projectsDir, os.ModePerm)
-	if err != nil {
-		fmt.Println("Error creating projects directory:", err)
-		return
-	}
+    // Ensure the "projects" subdirectory exists
+    projectsDir := filepath.Join(projectDir, "projects")
+    err = os.MkdirAll(projectsDir, os.ModePerm)
+    if err != nil {
+        fmt.Println("Error creating projects directory:", err)
+        return
+    }
 
-	fmt.Println("NeuralForge and projects directories are ready.")
+    fmt.Println("NeuralForge and projects directories are ready.")
 }
 
-// Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+    return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
-//-------------------------------
-
-// CreateNeuralForgeFolder creates a folder called "NeuralForge" in the user's home directory
 func (a *App) CreateProjectFolder() (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        return "", err
+    }
 
-	NeuralForgeDir := filepath.Join(homeDir, "NeuralForge")
+    NeuralForgeDir := filepath.Join(homeDir, "NeuralForge")
 
-	err = os.MkdirAll(NeuralForgeDir, os.ModePerm)
-	if err != nil {
-		return "", err
-	}
+    err = os.MkdirAll(NeuralForgeDir, os.ModePerm)
+    if err != nil {
+        return "", err
+    }
 
-	return NeuralForgeDir, nil
+    return NeuralForgeDir, nil
 }
 
-// ListProjects lists all folders in the NeuralForge/projects/ directory
 func (a *App) ListProjects() ([]string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, err
-	}
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        return nil, err
+    }
 
-	projectsDir := filepath.Join(homeDir, "NeuralForge", "projects")
-	folders, err := ioutil.ReadDir(projectsDir)
-	if err != nil {
-		return nil, err
-	}
+    projectsDir := filepath.Join(homeDir, "NeuralForge", "projects")
+    folders, err := ioutil.ReadDir(projectsDir)
+    if err != nil {
+        return nil, err
+    }
 
-	var projectNames []string
-	for _, folder := range folders {
-		if folder.IsDir() {
-			projectNames = append(projectNames, folder.Name())
-		}
-	}
+    var projectNames []string
+    for _, folder := range folders {
+        if folder.IsDir() {
+            projectNames = append(projectNames, folder.Name())
+        }
+    }
 
-	return projectNames, nil
+    return projectNames, nil
 }
 
-// CreateProject creates a new project folder in the user's NeuralForge/projects/ directory
 func (a *App) CreateProject(projectName string) (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        return "", err
+    }
 
-	projectDir := filepath.Join(homeDir, "NeuralForge", "projects", projectName)
-	err = os.MkdirAll(projectDir, os.ModePerm)
-	if err != nil {
-		return "", err
-	}
+    projectDir := filepath.Join(homeDir, "NeuralForge", "projects", projectName)
+    err = os.MkdirAll(projectDir, os.ModePerm)
+    if err != nil {
+        return "", err
+    }
 
-	return projectDir, nil
+    return projectDir, nil
 }
--- main.go
+++ main.go
@@ -1,36 +1,104 @@
 package main
 
 import (
-	"embed"
+    "embed"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+    "github.com/gofiber/fiber/v2"
+    "github.com/joho/godotenv"
+    "github.com/wailsapp/wails/v2"
+    "github.com/wailsapp/wails/v2/pkg/options"
+    "github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
+    // Load .env file if it exists
+    err := godotenv.Load()
+    if err != nil {
+        fmt.Println("No .env file found, using default settings")
+    }
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "NeuralForge",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+    app := NewApp()
 
-	if err != nil {
-		println("Error:", err.Error())
+    // Check if we should run in server mode
+    serverMode := os.Getenv("SERVER_MODE")
+    if serverMode == "true" {
+        runServerMode(app)
+    }else{
+		runDesktopMode(app)
 	}
+
+    
+}
+
+func runServerMode(app *App) {
+    fiberApp := fiber.New()
+
+    // Set up routes
+    setupRoutes(fiberApp, app)
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    fmt.Printf("Server running on http://localhost:%s\n", port)
+    log.Fatal(fiberApp.Listen(":" + port))
+}
+
+func runDesktopMode(app *App) {
+    // Create application with options
+    err := wails.Run(&options.App{
+        Title:  "NeuralForge",
+        Width:  1024,
+        Height: 768,
+        AssetServer: &assetserver.Options{
+            Assets: assets,
+        },
+        BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+        OnStartup:        app.startup,
+        Bind: []interface{}{
+            app,
+        },
+    })
+
+    if err != nil {
+        println("Error:", err.Error())
+    }
+}
+
+func setupRoutes(fiberApp *fiber.App, appLogic *App) {
+    fiberApp.Get("/api/greet/:name", func(c *fiber.Ctx) error {
+        name := c.Params("name")
+        return c.SendString(appLogic.Greet(name))
+    })
+
+    fiberApp.Get("/api/list-projects", func(c *fiber.Ctx) error {
+        projects, err := appLogic.ListProjects()
+        if err != nil {
+            return c.Status(500).SendString(err.Error())
+        }
+        return c.JSON(projects)
+    })
+
+    fiberApp.Post("/api/create-project", func(c *fiber.Ctx) error {
+        var body struct {
+            ProjectName string `json:"projectName"`
+        }
+        if err := c.BodyParser(&body); err != nil {
+            return c.Status(400).SendString(err.Error())
+        }
+        projectDir, err := appLogic.CreateProject(body.ProjectName)
+        if err != nil {
+            return c.Status(500).SendString(err.Error())
+        }
+        return c.SendString(projectDir)
+    })
+
+    // Add more routes as needed
 }
